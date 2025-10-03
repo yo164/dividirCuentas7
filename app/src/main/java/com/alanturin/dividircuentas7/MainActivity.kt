d7ef8c0377be252daa9ce89d70dc38541488493d
Francisco de Asís Cobo Sánchez <fcobsan076@cpifpalanturing.es>
2025-10-03 08:37:01 +0200

    guardado clase

--- app/src/main/java/com/alanturin/dividircuentas7/MainActivity.kt
+++ app/src/main/java/com/alanturin/dividircuentas7/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
@@ -110,7 +111,12 @@ class MainActivity : ComponentActivity() {
 
                         Spacer(modifier = Modifier.height(24.dp))
 
-                        Button(onClick = {
+
+                        var activaBoton = !(cuenta.isEmpty() || comensales.isEmpty())
+                        Button(
+                            modifier = Modifier.fillMaxWidth(),
+                            enabled = activaBoton,
+                            onClick = {
                             total = hacerCuenta(
                                 cantidad = cuenta,
                                 sliderPosition = sliderPosition,
